846546cc2de3519a3e8195dcf9bc70b6a7edc85c
1990minds <1990minds@gmail.com>
2022-06-24 10:52:16 +0530

    kiran

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -4,7 +4,7 @@ import {useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {fetchlogin, authenticateSelector} from '../../api/authSlice'
 import { Link } from "react-router-dom";
-import Logo from '../../assets/MODSIM.png'
+import Logo from '../../assets/MODSIMbeta.png'
 import Backgroundimage from '../../assets/bg.jpg'
 import {
   Layout,
@@ -155,7 +155,7 @@ export default function Login({history}) {
               span={8}
               style={{ marginTop: '10%', }}
               >
-                <img src={Logo} alt='Logo' style={{ width: "auto", height: '30px', display: 'block', marginLeft: 'auto', marginRight: 'auto', }}/>
+                <img src={Logo} alt='Logo' style={{ width: "auto", height: '80px', display: 'block', marginLeft: 'auto', marginRight: 'auto', }}/>
                 <div style={{ padding: '60px'}}>
                 {/* <Title className="mb-15">Login</Title> */}
                 {/* <Title className="font-regular text-muted" level={5}>
