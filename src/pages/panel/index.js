8212551f4d7e86cae35bfc6f6a544dfe571a1cae
sfodevbrl <sfodevbrl@gmail.com>
2022-06-29 14:02:08 +0530

    msg

--- src/pages/panel/index.js
+++ src/pages/panel/index.js
@@ -2,7 +2,7 @@
 import React, { useState, useLayoutEffect} from 'react'
 import Layout from '../../components/layout/Main'
 import PanelTable from './paneltable'
-import {fetchAllpanel, fetchProductPanels,panelSelector} from '../../api/panel'
+import {fetchProductPanels,panelSelector} from '../../api/panel'
 import {useDispatch, useSelector} from 'react-redux'
 import { useEffect } from 'react'
 import Createpanel from './createpanel'
