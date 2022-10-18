8348e30abf6d143c2657a21191b6b7156c38a8ff
1990minds <1990minds@gmail.com>
2022-10-18 16:25:46 +0530

    s

--- src/pages/projects/quotation.js
+++ src/pages/projects/quotation.js
@@ -10,6 +10,7 @@ import {useParams} from 'react-router-dom'
 import Item from 'antd/lib/list/Item';
 import { PercentageOutlined } from '@ant-design/icons';
 import { getCurrencyList } from 'currency-map-country';
+import ReactQuill from 'react-quill';
 const { TextArea } = Input;
 const { Option } = Select;
 
@@ -30,7 +31,9 @@ export default function Quotation({current_project,cancel}) {
   const [validityYear, setYear]=useState(null)
   const [validityMonth, setMonth]=useState(null)
   const {id} = useParams()
-     
+  const [terms, setTerms] = useState('');
+  const [notes, setNotes] = useState('');
+
 
   const onFinish = (values) => {
     
@@ -40,6 +43,8 @@ export default function Quotation({current_project,cancel}) {
         packing_forwarding:values.packing_forwarding,
         freight:values.freight,
         insurance:values.insurance,
+        // terms_conditions:terms,
+        // note:notes,
         terms_conditions:values.terms_conditions,
         note:values.note,
         electrical_components:values.electrical_components,
@@ -226,8 +231,10 @@ export default function Quotation({current_project,cancel}) {
                   rows={4}
                   style={{
                   height: 40,
-    }}
+                     }}
                   onChange={onChange} />
+                  {/* <ReactQuill theme="snow"  rows={4} value={terms} onChange={setTerms}/> */}
+
                   </Form.Item>
                   </Col>
                   </Row>
@@ -242,12 +249,14 @@ export default function Quotation({current_project,cancel}) {
                       rules={[{ required: true, message: 'Please enter Note' }]}
                     >
                       <TextArea   showCount
-                maxLength={600}
-                rows={4}
-                style={{
-                height: 40,
-                }}
-                onChange={onChange} />
+                      maxLength={600}
+                      rows={4}
+                      style={{
+                      height: 40,
+                      }}
+                      onChange={onChange} />
+                  {/* <ReactQuill theme="snow"  rows={4} value={notes} onChange={setNotes}/> */}
+
                     </Form.Item>
                   </Col>
 
