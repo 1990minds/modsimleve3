ce40de7cfea336f07680b2a534d38276ddfcae83
1990Minds <1990minds@gmail.com>
2023-01-27 18:01:56 +0530

    msg

--- src/key.js
+++ src/key.js
@@ -1,8 +1,8 @@
 const  keyUri = {
 
-    // BACKEND_URI:'https://my-app-5gv32.ondigitalocean.app/api'
+    BACKEND_URI:'https://my-app-5gv32.ondigitalocean.app/api'
 
-    BACKEND_URI:'http://localhost:5000/api'
+    // BACKEND_URI:'http://localhost:5000/api'
 }
 
 let token = localStorage.getItem('token')
